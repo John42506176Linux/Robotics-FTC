471b8fd4724b7e15fd4074b31f0554e51b6b50aa
John42506176Linux <johnkongtcheu@gmail.com>
2018-04-03 15:12:17 -0400

    Fixed Url

---
+++ JSVSecure/app/src/main/java/com/example/jsvsecure/AzureServiceAdapter.java
@@ -0,0 +1,38 @@
+package com.example.jsvsecure;
+
+
+import android.content.Context;
+import com.microsoft.windowsazure.mobileservices.*;
+
+public class AzureServiceAdapter {
+    private String mMobileBackendUrl = "https://jsvsecure.azurewebsites.net";
+    private Context mContext;
+    private MobileServiceClient mClient;
+    private static AzureServiceAdapter mInstance = null;
+
+    private AzureServiceAdapter(Context context) {
+        mContext = context;
+        mClient = new MobileServiceClient(mMobileBackendUrl, mContext);
+    }
+
+    public static void Initialize(Context context) {
+        if (mInstance == null) {
+            mInstance = new AzureServiceAdapter(context);
+        } else {
+            throw new IllegalStateException("AzureServiceAdapter is already initialized");
+        }
+    }
+
+    public static AzureServiceAdapter getInstance() {
+        if (mInstance == null) {
+            throw new IllegalStateException("AzureServiceAdapter is not initialized");
+        }
+        return mInstance;
+    }
+
+    public MobileServiceClient getClient() {
+        return mClient;
+    }
+
+    // Place any public methods that operate on mClient here.
+}
